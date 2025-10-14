28c72b95d23756d1561f5f25ef93b46b48e49048
Isaac Wheeler <ickaser@gmail.com>
2025-10-14 14:40:25 -0400

    Take out actual optimization dependencies, at least for now, in favor of least squares

--- src/MicrowaveLyoModeling.jl
+++ src/MicrowaveLyoModeling.jl
@@ -11,7 +11,6 @@ using Reexport
 @reexport using CSV
 @reexport using TypedTables
 @reexport using NonlinearSolve
-@reexport using OptimizationOptimJL
 @reexport using SavitzkyGolay
 @reexport using LaTeXStrings
 @reexport using DataInterpolations: LinearInterpolation, ExtrapolationType, ConstantInterpolation
@@ -21,14 +20,6 @@ using PrecompileTools
 using SpecialFunctions: besselj0, besselj1
 using Roots
 using Dates
-
-using LineSearches
-optalg = LBFGS(linesearch=LineSearches.BackTracking())
-export optalg
-const objf_KBB = OptimizationFunction(LyoPronto.obj_pd, AutoForwardDiff(chunksize=3))
-const objf_Rp = OptimizationFunction(LyoPronto.obj_pd, AutoForwardDiff(chunksize=3))
-const objf_KRp = OptimizationFunction(LyoPronto.obj_pd, AutoForwardDiff(chunksize=4))
-export objf_KBB, objf_Rp, objf_KRp
 
 include(srcdir("plotrecipes.jl"))
 include(srcdir("braatz.jl"))
