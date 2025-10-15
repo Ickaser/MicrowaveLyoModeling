599e36c6cad4d8a5ed2f6864aacc8da92bfc8fc0
Isaac Wheeler <ickaser@gmail.com>
2025-10-15 17:02:19 -0400

    Add LevelSet scripts

---
+++ scripts/LS_M1.jl
@@ -0,0 +1,217 @@
+@quickactivate :LevelSetSublimation
+const LSS = LevelSetSublimation
+using UnitfulLatexify
+
+# plot defaults
+default(:linewidth, 3)
+default(:markersize, 5)
+default(:fontfamily, "Computer Modern")
+
+# -----------------------
+
+begin
+base_props = LSS.base_props
+
+# Get some stuff from LyoProntoNIIMBLRF
+lcfitparams = load(datadir("exp_pro", "M1_KvRpRF.jld2"))
+@unpack Kvwf, Bf, Bvw = lcfitparams
+expdat = load(datadir("exp_pro", "M1_processed.jld2"))
+thmdat = expdat["thm_pd"]
+t_end = expdat["t_end"]
+
+# ---- Properties which do not change in time
+vialsize = "6R"
+fillvol = 5u"mL"
+# Mass transfer
+c_solid = 0.05u"g/mL"
+ρ_solution = 1.0u"g/mL"
+ϵ = (ρ_solution-c_solid)/ρ_solution 
+κ = 0.0u"m^2" # no viscous-regime flow allowed
+# R_p values to mass transfer
+Rp0 = 1.4u"cm^2*hr*Torr/g"
+A1 = 16u"cm*hr*Torr/g"
+Tguess = 260u"K"
+l = sqrt(u"R"*Tguess/base_props.Mw) / A1
+# Heat transfer
+kd = LSS.k_sucrose * (1-ϵ)
+m_v = LyoPronto.get_vial_mass(vialsize)
+A_v = π*LyoPronto.get_vial_radii(vialsize)[2]^2
+# Microwave
+B_d = 0.0u"Ω/m^2"
+tcprops = TimeConstantProperties(ϵ, l, κ, Rp0, kd, Kvwf, m_v, A_v, B_d, Bf, Bvw)
+
+# ------- Properties which may change in time
+f_RF = RampedVariable(8.0u"GHz")
+pch = RampedVariable(100.0u"mTorr")
+Tsh = RampedVariable(uconvert.(u"K", [-40.0, 10]*u"°C"), 1u"K/minute")
+P_per_vial = RampedVariable(10u"W"/17 * 0.54)
+# Heat transfer coefficient as function of pressure
+KC = 2.75e-4u"cal/s/K/cm^2"
+KP = 8.93e-4u"cal/s/K/cm^2/Torr"
+KD = 0.46u"1/Torr"
+Kshf = RpFormFit(KC, KP, KD)
+tvprops = TimeVaryingProperties(f_RF, P_per_vial, Tsh, pch, Kshf)
+
+# paramsd = LSS.base_props, tcprops, tvprops
+paramsd = LSS.base_props, tcprops, tvprops
+
+# ------------
+# Assemble simulation
+simgridsize = (41, 31)
+
+# config = Dict{Symbol, Any}()
+# @pack! config = paramsd, vialsize, fillvol, simgridsize
+config = @ntuple paramsd vialsize fillvol simgridsize
+config = merge(config, (time_integ=Val(:dae_then_exp),))
+end
+
+# Run simulation
+
+# @time res, fname = produce_or_load(sim_from_dict, config; filename=hash, verbose=true, tag=true, prefix=datadir("sims", "M1"))
+@info "before solve"
+
+@time res = sim_from_dict(config; verbose=true)
+
+@info "after solve"
+fname = datadir("sims", "M1_"*string(hash(config), base=10)*".jld2")
+safesave(fname, res)
+
+# fname = datadir("sims", "M1_"*string(hash(config), base=10)*".jld2")
+fname = datadir("sims", "M1_8944706271621697748.jld2")
+res = load(fname)
+
+sim = res["sim"]
+@unpack dom = sim
+
+# --------------
+
+# Plot things of interest
+
+# ---------------------------
+# Plots
+
+# summaryT(sim, tstart=0.01, tend=0.99, layout=(2,3), clims=(-30, 50))
+# plot!(size=(800,300), bottom_margin=10Plots.px)
+# savefig(plotsdir("M1_LSS_summary.svg"))
+# savefig(plotsdir("M1_LSS_summary.pdf"))
+
+locs = [(0.0, 0.0), (0.8, 0.05), (0.2, 0.5)]
+vtmarks = [:diamond, :utriangle, :square, :circle]
+
+resetfontsizes(); scalefontsizes(1.2)
+begin
+pl_sum = summaryT(sim, tstart=0.10, tend=0.80, layout=(1,3))
+placethermocouples!(dom, locs, c=palette(:Oranges_4)[4:-1:2], markers=vtmarks, label="", markersize=8);
+placethermocouples!(dom, [(0.95, 0.9)], msc=palette(:Oranges_4)[4:4], c=:white, markers=[:circle], label="", markersize=8, msw=4);
+plot!(size=(600, 200), left_margin=-5Plots.px, right_margin=0Plots.px)
+plot!(pl_sum[4], cbar_title="\nTemperature [°C]", right_margin=20Plots.px)
+end
+# savefig(plotsdir("M1_LSS_summVT.svg"))
+# savefig(plotsdir("M1_LSS_summVT.pdf"))
+
+# resetfontsizes()
+begin
+labs = vcat([L"$T$, model "*i for i in ["bottom","corner","center"]], L"$T_\mathrm{vw}$, model")
+pl_T = blankplothrC(;xlabel=L"t", )
+plot!(Tsh, c=:black, tmax=t_end, label=L"T_\mathrm{sh}")
+@df thmdat exptfplot!(:t, :T4, :T1, sampmarks=true, linealpha=0.2, nmarks=20)
+@df thmdat exptvwplot!(:t, :T3, nmarks=25, msw=3, )
+vt_plot!(sim, locs; labels=permutedims(labs), markers=permutedims(vtmarks), step=40, samplemarkers=true, linealpha=0.8)
+tendplot!(t_end, label="")
+plot!(legend=:outerright, size=(700, 300), bottom_margin=15Plots.px, left_margin=15Plots.px)
+end
+# savefig(plotsdir("M1_multiT.svg"))
+# savefig(plotsdir("M1_multiT.pdf"))
+# plot!(pl_sum, margin_right=26Plots.px)
+plot(pl_T, pl_sum, layout=@layout([a{0.6h}; b]), size=(700,500))
+savefig(plotsdir("M1_allLSS.svg"))
+savefig(plotsdir("M1_allLSS.pdf"))
+
+begin
+resetfontsizes(); scalefontsizes(1.5)
+pl_sum = summaryT(sim, tstart=0.10, tend=0.80, layout=(1,3))
+placethermocouples!(dom, locs, c=palette(:Oranges_4)[4:-1:2], markers=vtmarks, label="", markersize=8);
+placethermocouples!(dom, [(0.95, 0.9)], msc=palette(:Oranges_4)[4:4], c=:white, markers=[:circle], label="", markersize=8, msw=4);
+plot!(size=(600, 200),  left_margin=-20Plots.px, right_margin=-5Plots.px)
+plot!(pl_sum[4], cbar_title="Temperature [°C]", left_margin=0Plots.px, right_margin=20Plots.px)
+labs = vcat(["LS "*i for i in ["bottom","corner","center"]], "LS vw")
+pl_T = blankplothrC(;)
+plot!(Tsh, c=:black, tmax=t_end, label="shelf")
+@df thmdat exptfplot!(:t, :T4, sampmarks=true, linealpha=0.2, nmarks=20, label="exp f")
+@df thmdat exptvwplot!(:t, :T3, nmarks=25, msw=3, label="exp vw")
+vt_plot!(sim, locs; labels=permutedims(labs), markers=permutedims(vtmarks), step=40, samplemarkers=true, linealpha=0.7)
+tendplot!(t_end, label="")
+plot!(legend=:outerbottomright, size=(700, 300), bottom_margin=15Plots.px, left_margin=15Plots.px)
+plot(pl_T, pl_sum, layout=@layout([a{0.6h}; b]), size=(700,500))
+end
+savefig(plotsdir("M1_allLSS_poster.svg"))
+
+begin
+pl_vtloc, T = plotframe(10*60*60, sim)
+placethermocouples!(sim.dom, locs, c=palette(:Oranges_4)[4:-1:2], markers=vtmarks, label="", markersize=8);
+# texts = [text(L"$T_\textrm{f%$i}$", 10) for i in 1:3]
+# labelthermocouples!(sim.dom, locs, texts)
+plot!(xlabel="", cbar=nothing)
+# annotate!(pl, dom.rmax.*locx, dom.zmax.*locy, texts)
+plot!(size=(240,200), colorbar_title="T [°C]")
+end
+# savefig(pl, plotsdir("M1_virtual_thermocouple_small.svg"))
+# savefig(pl, plotsdir("M1_virtual_thermocouple_small.pdf"))
+
+# --------- Compute effective Rp
+
+t_Rp, hd_eff, Rp_eff, Asub = get_eff_Rp(sim);
+Rp_orig = @. Rp0 + A1*hd_eff
+relerr = (Rp_eff .- Rp_orig)./Rp_orig
+begin
+pl1 = plot(u"cm", u"cm^2*Torr*hr/g", ylabel="R_p", xlabel=nothing, xticks=(0:0.5:1.5, []), unitformat=latexsquareunitlabel)
+plot!(hd_eff, Rp_eff, seriestype=:samplemarkers, marker=:circle, label="LSS with curvature", ylims=(0, 40))
+plot!(hd_eff, Rp_orig, c=:black, label="Original Rp")
+pl2 = plot(u"cm", NoUnits, ylabel="Relative\nDifference", xlabel=LaTeXString("h_d"), unitformat=latexsquareunitlabel)
+hline!([0], label="", c=:black, lw=1)
+plot!(hd_eff, relerr; c=1, ylim=(-0.06, 0.06), label="",)
+pl3 = plot(u"cm", u"cm^2", ylabel="A_\\mathrm{sub}", xlabel=LaTeXString("h_d"), unitformat=latexsquareunitlabel)
+plot!(hd_eff, Asub, ylim=(3.1, 3.25), label="")
+plot(pl1, pl2, pl3, layout=@layout([a{0.5h}; b; c]), link=:x, size=(600, 600))
+end
+savefig(plotsdir("M1_curvatureRp.svg"))
+savefig(plotsdir("M1_curvatureRp.pdf"))
+
+# -------- LCLyo model
+
+A_p, A_v = π.* get_vial_radii("6R").^2
+c_solid = 0.05u"g/mL"
+ρ_solution = 1.0u"g/mL"
+LCparams = [
+    (RpFormFit(Rp0, A1, 0u"1/cm"), fillvol/A_p, c_solid, ρ_solution),
+    (Kshf, A_v, A_p),
+    (pch, Tsh, P_per_vial),
+    (fillvol*ρ_solution, base_props.Cpf, m_v, base_props.cp_vw),
+    (f_RF(0), base_props.εppf, base_props.εpp_vw),
+    (Kvwf, Bf, Bvw)
+]
+LCParams = ParamObjRF(LCparams)
+
+lcprob = ODEProblem(LCParams)
+lclyo = solve(lcprob, Rodas3())
+
+begin
+blankplothrC()
+@df thmdat exptfplot!(:t, :T1, :T4, marker=:circle, )
+@df thmdat exptvwplot!(:t, :T3, marker=:square, )
+modrftplot!(lclyo, labels=permutedims([i*", LC" for i in ["\$T_f\$", "\$T_{vw}\$"]]))
+plot!(size=(400, 200), legend=:outerright)
+end
+# savefig(plotsdir("M1_lc_small.svg"))
+# savefig(plotsdir("M1_lc_small.pdf"))
+
+begin
+pl_comp = blankplothrC()
+@df thmdat exptfplot!(:t, :T1, :T4, marker=:circle, )
+@df thmdat exptvwplot!(:t, :T3, marker=:square, )
+vt_plot!(sim, locs; labels=permutedims(labs), markers=permutedims(vtmarks), step=40)
+modrftplot!(lclyo, c=:green, linealpha=0.5, labels=permutedims([i*", LC" for i in ["\$T_f\$", "\$T_{vw}\$"]]))
+tendplot!(t_end)
+plot!(legend=:outerright, size=(600, 200), bottom_margin=15Plots.px, left_margin=15Plots.px)
+plot(pl_comp, pl_vtloc, layout=@layout([a{0.8w} b]), size=(800,300))
+end
---
+++ scripts/LS_error_order.jl
@@ -0,0 +1,233 @@
+using DrWatson
+@quickactivate :MicrowaveLyoModeling
+
+# plot defaults
+default(:linewidth, 3)
+default(:markersize, 5)
+default(:fontfamily, "Computer Modern")
+
+# -----------------------
+# Set up physical parameters
+begin
+
+base_props = LevelSetSublimation.base_props
+
+# Get some stuff from lumped capacitance model
+lcfitparams = load(datadir("exp_pro", "M1_KvRpRF.jld2"))
+@unpack Kvwf, Bf, Bvw = lcfitparams
+expdat = load(datadir("exp_pro", "M1_processed.jld2"))
+thmdat = expdat["thm_pd"]
+t_end = expdat["t_end"]
+
+vialsize = "6R"
+fillvol = 5u"mL"
+# Mass transfer
+c_solid = 0.05u"g/mL"
+ρ_solution = 1.0u"g/mL"
+ϵ = (ρ_solution-c_solid)/ρ_solution 
+κ = 0.0u"m^2" # no viscous-regime flow allowed
+# R_p values to mass transfer
+Rp0 = 1.4u"cm^2*hr*Torr/g"
+A1 = 16u"cm*hr*Torr/g"
+Tguess = 260u"K"
+l = sqrt(u"R"*Tguess/base_props.Mw) / A1
+# Heat transfer
+kd = LyoPronto.k_sucrose * (1-ϵ)
+m_v = LyoPronto.get_vial_mass(vialsize)
+A_v = π*LyoPronto.get_vial_radii(vialsize)[2]^2
+# Microwave
+B_d = 0.0u"Ω/m^2"
+tcprops = TimeConstantProperties(ϵ, l, κ, Rp0, kd, Kvwf, m_v, A_v, B_d, Bf, Bvw)
+
+f_RF = RampedVariable(8.0u"GHz")
+pch = RampedVariable(100.0u"mTorr")
+Tsh = RampedVariable(uconvert.(u"K", [-40.0, 10]*u"°C"), 1u"K/minute")
+P_per_vial = RampedVariable(10u"W"/17 * 0.54)
+# Heat transfer coefficient as function of pressure
+KC = 2.75e-4u"cal/s/K/cm^2"
+KP = 8.93e-4u"cal/s/K/cm^2/Torr"
+KD = 0.46u"1/Torr"
+Kshf = RpFormFit(KC, KP, KD)
+tvprops = TimeVaryingProperties(f_RF, P_per_vial, Tsh, pch, Kshf)
+
+paramsd = (base_props, tcprops, tvprops)
+end
+
+# ------------
+# Assemble simulation
+simgridsizes = [
+                (21, 21),
+                (31, 31),
+                (41, 41),
+                (51, 51),
+                (61, 61),
+                (71, 71),
+                # (41, 31),
+                # (51, 31),
+                # (61, 31),
+                # (71, 31),
+                # (31, 21),
+                # (31, 41),
+                # (31, 51),
+                # (31, 61),
+                # (31, 71),
+]
+
+# config = Dict{Symbol, Any}()
+# @pack! config = paramsd, vialsize, fillvol, simgridsize
+config = @dict paramsd vialsize fillvol
+config[:simgridsize] = simgridsizes
+config[:time_integ] = Val(:dae_then_exp)
+# config[:time_integ] = Val(:exp_newton)
+allconfigs = dict_list(config)
+nconfigs = [NamedTuple((Symbol(key),value) for (key,value) in d) for d in allconfigs]
+
+
+# Run simulations
+timing = map(nconfigs) do config
+    @elapsed produce_or_load(sim_from_dict, config; filename=hash, verbose=true, tag=false, prefix=datadir("sims", "err"))
+end
+
+#15-element Vector{Float64}: # out of order now
+#   206.9773286
+#    48.5550164
+#    65.7792836
+#   179.6836682
+#   269.2749256
+#   342.0172003
+#   916.4128808
+#   142.8181078
+#   217.4506176
+#   246.0629959
+#   340.6295969
+#   225.4222626
+#   562.6863109
+#  1651.0493648
+#  3891.9363282
+
+# For only the n=n grid sizes
+#   111.1484467
+#   124.0673761
+#   348.3150078
+#   645.7112252
+#  1407.6015413
+#  2713.8488763
+
+using DataFrames
+allsims = collect_results(datadir("sims"), rinclude=[r"err"])
+simtab = Table(map(eachrow(allsims)) do row
+    # if row.sim.config[:time_integ] == Val(:dae_then_exp)
+    #     row.sim = merge(row.sim, (Tf = nothing,))
+    # end
+    merge(row.sim, (path=row["path"],), NamedTuple(row.sim.config))
+end)
+
+best = simtab[argmax(prod.(simtab.simgridsize))];
+rbest = simtab[findmax(x->x[1], simtab.simgridsize)[2]];
+zbest = simtab[findmax(x->x[2], simtab.simgridsize)[2]];
+best.simgridsize
+
+locs = [(0.0, 0.0)]#, (0.8, 0.05), (0.2, 0.5)]
+t1 = range(0, 40000, length=80) # stop before ice detaches from wall
+t2 = range(41000, best.sol.t[end]*0.99, length=20) # pick up after
+Tbest1 = virtual_thermocouple(locs, t1, best)
+Tbest2 = virtual_thermocouple(locs, t2, best)
+# Tbestr = virtual_thermocouple(locs, t, rbest)
+# Tbestz = virtual_thermocouple(locs, t, rbest)
+function compare_Tf(sim, Tb, t)
+    Ts = virtual_thermocouple(locs, t, sim)
+    return sqrt(sum(abs2, Ts-Tb)/length(Ts))
+end
+function compare_Tvw(sim, best, t)
+    Ts = [sim.sol(ti).Tvw for ti in t]
+    Tb = [best.sol(ti).Tvw for ti in t]
+    return sqrt(sum(abs2, Ts-Tb)/length(Ts))
+end
+
+simtab_rz = filter(x->x.simgridsize[1]==x.simgridsize[2], simtab)
+
+errs = map(simtab_rz) do sim
+    err_Tf_rz1 = @time compare_Tf(sim, Tbest1, t1)
+    err_Tf_rz2 = @time compare_Tf(sim, Tbest2, t2)
+    # err_Tf_r = compare_Tf(sim, Tb=Tbestr)
+    # err_Tf_z = compare_Tf(sim, Tb=Tbestz)
+    err_t_rz = abs(sim.sol.t[end] - best.sol.t[end])/best.sol.t[end]
+    err_td_rz = abs(sim.sol.tsplit - best.sol.tsplit)/best.sol.tsplit
+    # err_t_r = abs(sim.sol.t[end] - rbest.sol.t[end])
+    # err_t_z = abs(sim.sol.t[end] - zbest.sol.t[end])
+    err_Tvw_rz1 = compare_Tvw(sim, best, t1)
+    err_Tvw_rz2 = compare_Tvw(sim, best, t2)
+    return merge((nr=sim.simgridsize[1], nz=sim.simgridsize[2]), @ntuple err_Tf_rz1 err_Tf_rz2 err_Tvw_rz1 err_Tvw_rz2 err_t_rz err_td_rz)
+end
+# @time compare_Tf(simtab[1])
+errtab = Table(simtab_rz, errs);
+sort!(errtab, by=x->x.nr*x.nz);
+
+cs = cgrad(:viridis)
+begin
+pl1 = blankplothrC(ylabel="Temperature difference to reference")
+# plot!(t, Tbest[:,1], c=cs[end], label="Reference")
+markers=[:circle, :heptagon, :utriangle, :dtriangle, :ltriangle, :rtriangle]
+for (i,sim) in enumerate(errtab)
+    c = cs[257 - sim.nr*sim.nz*256÷prod(best.simgridsize)]
+    mark = markers[(i-1)%6+1]
+    plot!(t1/3600, Tbest1[:,1] .- virtual_thermocouple([locs[1]], t1, sim), c=c, marker=mark, label=L"n_r=n_z=%$(sim.nr)")
+end
+plot!(xlim=(0, 11.3), ylim=(-0.2,0.2), legend=nothing, left_margin=20Plots.px)
+pl2 = blankplothrC(ylabel="")
+for (i,sim) in enumerate(errtab)
+    c = cs[257 - sim.nr*sim.nz*256÷prod(best.simgridsize)]
+    mark = markers[(i-1)%6+1]
+    plot!(t2/3600, Tbest2[:,1] .- virtual_thermocouple([locs[1]], t2, sim), c=c, marker=mark, label=L"n_r=n_z=%$(sim.nr)")
+end
+plot!(legend=:outerright)
+plot(pl1, pl2, size=(800, 400), bottom_margin=20Plots.px)
+end
+savefig(plotsdir("M1_LSS_error_over_time.svg"))
+savefig(plotsdir("M1_LSS_error_over_time.pdf"))
+
+resetfontsizes(); scalefontsizes(1.2)
+begin
+
+plTf = @df filter(x->(x.err_Tf_rz1>0), errtab) scatter(:nr, :err_Tf_rz1, scale=:log10, label="pre-detach")
+@df filter(x->(x.err_Tf_rz1>0), errtab) scatter!(:nr, :err_Tf_rz2, scale=:log10, marker=:square, label="post-detach")
+# @df filter(x->(x.nr==31 && x.rzerr>0), errtab) scatter!(:nr, :rzerr, scale=:log10, label="nr=31")
+plot!([10^1.3, 10^1.8], [10^0.2, 10^-0.8], label=L"err$\propto n^{-2}$", ls=:dash, c=:gray)
+plot!([10^1.3, 10^1.8], [10^-0.9, 10^-1.9], label="", ls=:dash, c=:gray)
+# plot!([10^1.4, 10^1.9], [10^0.01, 10^0.01], label="slope 0", ls=:dot, c=:gray)
+plot!(xlabel=L"grid points $n_r=n_z$", ylabel=L"RMS error in $T_\mathrm{f}$ [K]")
+
+plot!(legend=:topright, bottom_margin=20Plots.px)
+# end
+# savefig(plotsdir("M1_LSS_convergence_dae_Tf.svg"))
+# savefig(plotsdir("M1_LSS_convergence_dae_Tf.pdf"))
+
+# begin
+plTvw = @df filter(x->(x.err_Tvw_rz1>0), errtab) scatter(:nr, :err_Tvw_rz1, scale=:log10, label="pre-detach")
+@df filter(x->(x.err_Tvw_rz1>0), errtab) scatter!(:nr, :err_Tvw_rz2, scale=:log10, marker=:square, label="post-detach")
+# @df filter(x->(x.nr==31 && x.rzerr>0), errtab) scatter!(:nr, :rzerr, scale=:log10, label="nr=31")
+plot!([10^1.3, 10^1.8], [10^0.22, 10^-0.78], label=L"err$\propto n^{-2}$", ls=:dash, c=:gray)
+plot!([10^1.3, 10^1.8], [10^-0.55, 10^-1.55], label="", ls=:dash, c=:gray)
+# plot!([10^1.3, 10^1.8], [10^-0.9, 10^-1.4], label=L"err$\propto n^{-1}$", ls=:dot, c=:gray)
+plot!(xlabel=L"grid points $n_r=n_z$", ylabel=L"RMS error in $T_\mathrm{vw}$ [K]")
+plot!(legend=:topright, bottom_margin=20Plots.px)
+# end
+# savefig(plotsdir("M1_LSS_convergence_dae_Tvw.svg"))
+# savefig(plotsdir("M1_LSS_convergence_dae_Tvw.pdf"))
+
+# begin
+plt = @df filter(x->x.err_t_rz>0, errtab) scatter(:nz, :err_t_rz, scale=:log10, label=L"t_\mathrm{end}")
+@df filter(x->x.err_t_rz>0, errtab) scatter!(:nz, :err_td_rz, scale=:log10, marker=:square, label=L"t_\mathrm{detach}")
+# plot!([10^1.35, 10^1.85], [10^1.77, 10^1.77], label="slope 0", ls=:dot, c=:gray)
+plot!([10^1.3, 10^1.8], [10^-2.0, 10^-3.5], label=L"err$\propto n^{-3}$", ls=:dash, c=:gray)
+plot!(xlabel=L"grid points $n_r=n_z$", ylabel=L"relative error in $t$")
+plot!(legend=:bottom)
+
+plot!(bottom_margin=20Plots.px)
+end
+savefig(plotsdir("M1_LSS_convergence_dae_t.svg"))
+savefig(plotsdir("M1_LSS_convergence_dae_t.pdf"))
+
+plot(plTf, plTvw, plt, layout=(1,3), size=(1000, 400), left_margin=20Plots.px)
+savefig(plotsdir("M1_LSS_convergence_dae_all.svg"))
+savefig(plotsdir("M1_LSS_convergence_dae_all.pdf"))
